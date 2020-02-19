d8c2a9b0a0146b38cbb7b00621ef26cc76a47846
qlem <clement.dommerc@gmail.com>
2020-02-19 22:21:45 +0100

    commit

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,7 +2,5 @@
 
 bg=/tmp/bg_lock.png
 shotgun -f png $bg > /dev/null 2>&1
-[[ $? -ne 0 ]] && i3lock $@
 gm convert $bg -scale 10% -scale 1000% -colorspace Gray -gamma 0.6 $bg > /dev/null 2>&1
-[[ $? -ne 0 ]] && i3lock $@
 i3lock -i $bg $@
