36836936c4dd3226aae526c7747570699b87f63b
qlem <clement.dommerc@gmail.com>
2020-02-29 11:39:35 +0100

    commit

--- bin/multihead.sh
+++ bin/multihead.sh
@@ -3,12 +3,17 @@
 edp=eDP-1
 dp=DP-2
 hdmi=HDMI-1
+pos='--right-of'
+
+if [[ $# -ge 1 ]] && [[ "$1" == 'left' ]]; then
+    pos='--left-of'
+fi
 
 if [[ -n $(xrandr | grep "$dp2 connected") ]]; then
-    xrandr --output $edp --primary --auto --output $dp --right-of $edp --auto 
+    xrandr --output $edp --primary --auto --output $dp $pos $edp --auto
 elif [[ -n $(xrandr | grep "$hdmi connected") ]]; then
-    xrandr --output $edp --primary --auto --output $hdmi --right-of $edp --auto
-else 
+    xrandr --output $edp --primary --auto --output $hdmi $pos $edp --auto
+else
     xrandr --output $edp --primary --auto --output $dp --off --output $hdmi --off
 fi
 $HOME/bin/wallpaper.sh
