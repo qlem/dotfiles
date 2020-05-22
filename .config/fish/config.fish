f3a1a80b395b9fb7814e21effd8a9c2ef9b321f9
qlem <clement.dommerc@gmail.com>
2020-05-22 22:00:30 +0200

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,18 +4,3 @@ if status is-interactive
         exec startx -- -keeptty
     end
 end
-
-# start tmux
-# if status is-interactive
-    # if test -z "$TMUX"
-        # set id (tmux ls | grep -vm1 attached | cut -d: -f1)
-        # if test -z "$id"
-            # tmux new-session -s "main"
-            # if test $status -ne 0
-                # tmux new-session
-            # end
-        # else
-            # tmux attach-session -t "$id"
-        # end
-    # end
-# end
