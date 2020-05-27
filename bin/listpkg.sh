8be3a9848ea9dfb7595922aead3e92c175e55a30
qlem <clement.dommerc@gmail.com>
2020-05-27 16:42:36 +0200

    wip

--- bin/listpkg.sh
+++ bin/listpkg.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+export LANG=C
+export LC_ALL=C
 
 tmp=/tmp/listpkg
 mkdir $tmp
@@ -7,8 +10,8 @@ base=$tmp/base.txt
 pkg=$tmp/pkg.txt
 diff=$tmp/diff.txt
 
-export LANG=C
-export LC_ALL=C
+bold='\033[1m'
+reset='\033[0m'
 
 # get pkg from base
 pacman -Si base | grep 'Depends On' | cut -d: -f2 | awk '{$1=$1};1' | tr ' ' '\n' > $base
@@ -22,9 +25,15 @@ pacman -Qeq > $pkg
 
 # list installed pkg except pkg from base and base-devel
 diff $pkg $base > $diff
-sed -i -r '/^(<|>).*$/!d;s/^(< |> )(.*)$/\2/' $diff
-sort -o $diff $diff
+sed -i -r '/^(<|>).*$|^---$/!d;s/^(< |> )(.*)$/\2/' $diff
+echo -e "${bold}Installed packages:${reset}"
+while read line; do
+    if [[ "$line" == '---' ]]; then
+        echo -e "\n${bold}Non-explicitly installed packages:${reset}"
+    else
+        echo "$line"
+    fi
+done < "$diff"
 count=$(wc -l $diff | awk '{print $1}')
-cat $diff
-echo "-- $count installed packages"
+echo -e "\n${bold}-- $((count - 1)) installed packages${reset}"
 rm -r $tmp
--- bin/ptop.sh
+++ bin/ptop.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 echo -n 'process > '
 read pname
-[ -z "$pname" ] && exit 1
-plist=$(pgrep $pname)
-[ -z "$plist" ] && echo 'Process not found' && exit 1
+[[ -z "$pname" ]] && exit 1
+plist=$(pgrep "$pname")
+[[ -z "$plist" ]] && echo 'Process not found' && exit 1
 pid=$(echo "$plist" | dmenu -p 'Pid(s)')
-[ -z "$pid" ] && exit 1
-top -d 1 -p $pid -H
+[[ -z "$pid" ]] && exit 1
+top -d 1 -p "$pid" -H
