1d82dac26ad29a12cd07c112f718ae63a4003d2f
Clément Dommerc <clement.dommerc@gmail.com>
2021-03-14 23:11:35 +0100

    update

--- run.sh
+++ run.sh
@@ -1,65 +1,91 @@
 #!/bin/sh
 
-cp ~/.bashrc .
-cp ~/.tmux.conf .
-cp ~/.vimrc .
-cp ~/.xinitrc .
-cp ~/.Xresources .
+dir=$HOSTNAME
+dir_bin="$dir/bin"
+dir_config="$dir/.config"
+dir_local="$dir/.local"
+dir_dwm="$dir/dwm"
+dir_xorg="$dir/xorg"
 
-[[ ! -d "bin" ]] && mkdir bin
-cp ~/bin/*.sh bin/
+[[ ! -d $dir ]] && mkdir $dir
 
-[[ ! -d ".config" ]] && mkdir .config
+# files in home
+cp ~/.bashrc "$dir/"
+cp ~/.tmux.conf "$dir/"
+cp ~/.vimrc "$dir/"
+cp ~/.xinitrc "$dir/"
+cp ~/.Xresources "$dir/"
 
-[[ ! -d ".config/alacritty" ]] && mkdir .config/alacritty
-cp ~/.config/alacritty/alacritty.yml .config/alacritty/
+# script files
+[[ ! -d $dir_bin ]] && mkdir $dir_bin
+cp ~/bin/*.sh "$dir_bin/"
 
-[[ ! -d ".config/conky" ]] && mkdir .config/conky
-cp ~/.config/conky/conky.conf .config/conky/
+# -- config files --
+[[ ! -d $dir_config ]] && mkdir $dir_config
 
-[[ ! -d ".config/fish" ]] && mkdir .config/fish
-[[ ! -d ".config/fish/functions" ]] && mkdir .config/fish/functions
-cp ~/.config/fish/config.fish .config/fish/
-cp ~/.config/fish/functions/* .config/fish/functions/
+# alacritty
+[[ ! -d "$dir_config/alacritty" ]] && mkdir "$dir_config/alacritty"
+cp ~/.config/alacritty/alacritty.yml "$dir_config/alacritty/"
 
-[[ ! -d ".config/spectrwm" ]] && mkdir .config/spectrwm
-cp ~/.config/spectrwm/spectrwm.conf .config/spectrwm/
+# conky
+[[ ! -d "$dir_config/conky" ]] && mkdir "$dir_config/conky"
+cp ~/.config/conky/conky.conf "$dir_config/conky/"
 
-[[ ! -d "dwm" ]] && mkdir dwm
-cp ~/projects/dwm-6.2/config.h dwm/
-cp ~/projects/dwm-6.2/config.mk dwm/
-cp ~/projects/dwm-6.2/dwm-setstatus.c dwm/
-cp ~/projects/dwm-6.2/dwm.patch dwm/
+# fish
+[[ ! -d "$dir_config/fish" ]] && mkdir "$dir_config/fish"
+[[ ! -d "$dir_config/fish/functions" ]] && mkdir "$dir_config/fish/functions"
+cp ~/.config/fish/config.fish "$dir_config/fish/"
+cp ~/.config/fish/functions/* "$dir_config/fish/functions/"
 
-[[ ! -d ".config/i3" ]] && mkdir .config/i3
-cp ~/.config/i3/config .config/i3/
+# spectrwm
+[[ ! -d "$dir_config/spectrwm" ]] && mkdir "$dir_config/spectrwm"
+cp ~/.config/spectrwm/spectrwm.conf "$dir_config/spectrwm/"
 
-[[ ! -d ".config/i3status" ]] && mkdir .config/i3status
-cp ~/.config/i3status/config .config/i3status/
-cp ~/.config/i3status/wrapper.py .config/i3status/
+# i3
+[[ ! -d "$dir_config/i3" ]] && mkdir "$dir_config/i3"
+cp ~/.config/i3/config "$dir_config/i3/"
 
-[[ ! -d ".config/picom" ]] && mkdir .config/picom
-cp ~/.config/picom/picom.conf .config/picom/
+# i3status
+[[ ! -d "$dir_config/i3status" ]] && mkdir "$dir_config/i3status"
+cp ~/.config/i3status/config "$dir_config/i3status/"
+cp ~/.config/i3status/wrapper.py "$dir_config/i3status/"
 
-[[ ! -d ".config/rofi" ]] && mkdir .config/rofi
-cp ~/.config/rofi/*.rasi .config/rofi/
+# picom
+[[ ! -d "$dir_config/picom" ]] && mkdir "$dir_config/picom"
+cp ~/.config/picom/picom.conf "$dir_config/picom/"
 
-[[ ! -d ".config/qlstatus" ]] && mkdir .config/qlstatus
-cp ~/.config/qlstatus/qlstatus.conf .config/qlstatus/
+# rofi
+[[ ! -d "$dir_config/rofi" ]] && mkdir "$dir_config/rofi"
+cp ~/.config/rofi/*.rasi "$dir_config/rofi/"
 
-[[ ! -d ".config/redshift" ]] && mkdir .config/redshift
-cp ~/.config/redshift/redshift.conf .config/redshift/
+# qlstatus
+[[ ! -d "$dir_config/qlstatus" ]] && mkdir "$dir_config/qlstatus"
+cp ~/.config/qlstatus/qlstatus.conf "$dir_config/qlstatus/"
 
-[[ ! -d ".config/terminator" ]] && mkdir .config/terminator
-cp ~/.config/terminator/config .config/terminator/
+# redshift
+[[ ! -d "$dir_config/redshift" ]] && mkdir "$dir_config/redshift"
+cp ~/.config/redshift/redshift.conf "$dir_config/redshift/"
 
-[[ ! -d ".config/dunst" ]] && mkdir .config/dunst
-cp ~/.config/dunst/dunstrc .config/dunst/
+# terminator
+[[ ! -d "$dir_config/terminator" ]] && mkdir "$dir_config/terminator"
+cp ~/.config/terminator/config "$dir_config/terminator/"
 
-[[ ! -d "xorg" ]] && mkdir xorg
-cp /etc/X11/xorg.conf.d/*.conf xorg/
+# dunst
+[[ ! -d "$dir_config/dunst" ]] && mkdir "$dir_config/dunst"
+cp ~/.config/dunst/dunstrc "$dir_config/dunst/"
+
+# xorg conf files
+[[ ! -d $dir_xorg ]] && mkdir $dir_xorg
+cp /etc/X11/xorg.conf.d/*.conf "$dir_xorg/"
+
+# dwm files
+[[ ! -d $dir_dwm ]] && mkdir $dir_dwm
+cp ~/projects/dwm-6.2/config.h "$dir_dwm/"
+cp ~/projects/dwm-6.2/config.mk "$dir_dwm/"
+cp ~/projects/dwm-6.2/dwm-setstatus.c "$dir_dwm/"
+cp ~/projects/dwm-6.2/dwm.patch "$dir_dwm/"
 
 # custom hhkb keyboard layout
-[[ ! -d ".local/share/xkb" ]] && mkdir -p .local/share/xkb
-cp ~/.local/share/xkb/hhkb-custom-layout.xkb .local/share/xkb/
-cp /etc/udev/rules.d/99-keyboard.rules .
+[[ ! -d "$dir_local/share/xkb" ]] && mkdir -p "$dir_local/share/xkb"
+cp ~/.local/share/xkb/hhkb-custom-layout.xkb "$dir_local/share/xkb/"
+cp /etc/udev/rules.d/99-keyboard.rules "$dir/"
