4b06fc1592a95886454f476ea9189e258e741559
Clément Dommerc <clement.dommerc@gmail.com>
2025-02-10 18:06:49 -0500

    refresh

--- x1c/config/nvim/init.lua
+++ x1c/config/nvim/init.lua
@@ -4,11 +4,37 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.mouse = ""
+-- vim.opt.fillchars = "lastline:>"
+
+-- line break
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 
 -- tabs
+vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4 
 vim.opt.shiftwidth = 4
 
+-- search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- indent
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- todo
+-- showmatch, matchtime
+-- statusline, ruler, laststatus, etc
+-- timeout, timeoutlen
+-- comments
+
 -- keymaps
 vim.api.nvim_set_keymap("n", "<C-)>", "<C-]>", { noremap = true })        
+
+-- a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line 
