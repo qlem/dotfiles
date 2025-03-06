557949cefbd441e9d6aa807a3edd9c7905683a9a
Clément Dommerc <clement.dommerc@gmail.com>
2025-03-05 20:10:52 -0500

    refresh

--- x1c/bin/audio.sh
+++ x1c/bin/audio.sh
@@ -4,7 +4,6 @@ export LC_ALL=C
 
 inc=5
 limit=150
-notifID=999942
 app='PulseAudio'
 icon_up='notification-audio-volume-high'
 icon_down='notification-audio-volume-medium'
@@ -17,14 +16,22 @@ volume=$(pactl list sinks | grep "Name: $sink" -A 7 | tail -n 1 | awk '{print $5
 case "$1" in
     '--volume-up')
         if (( volume + inc <= limit )) && pactl set-sink-volume "$sink" "+$inc%"; then
-            dunstify -a "$app" -u low -r "$notifID" -i "$icon_up" "$app" "Volume   UP:  $(( volume + inc ))%"
+            dunstify -a "$app" \
+                -u low \
+                -h string:x-dunst-stack-tag:pavolume \
+                -i "$icon_up" \
+                "$app" "Volume   UP:  $(( volume + inc ))%" 
         fi
         ;;
 
     '--volume-down')
         if (( volume > 0 )) && pactl set-sink-volume "$sink" "-$inc%"; then
             (( volume - inc < 0 )) && volume=0 || volume=$(( volume - inc ))
-            dunstify -a "$app" -u low -r "$notifID" -i "$icon_down" "$app" "Volume DOWN:  $volume%"
+            dunstify -a "$app" \
+                -u low \
+                -h string:x-dunst-stack-tag:pavolume \
+                -i "$icon_down" \
+                "$app" "Volume DOWN:  $volume%"
         fi
         ;;
 
@@ -32,9 +39,17 @@ case "$1" in
         if pactl set-sink-mute "$sink" toggle; then
             muted=$(pactl list sinks | grep "Name: $sink" -A 6 | tail -n 1 | awk '{print $2}')
             if [[ "$muted" == 'yes' ]]; then
-                dunstify -a "$app" -u normal -r "$notifID" -i "$icon_mute" "$app" "Audio OFF"
+                dunstify -a "$app" \
+                    -u normal \
+                    -h string:x-dunst-stack-tag:pavolume \
+                    -i "$icon_mute" \
+                    "$app" "Audio OFF"
             else
-                dunstify -a "$app" -u normal -r "$notifID" -i "$icon_unmute"  "$app" "Audio ON:  $volume%"
+                dunstify -a "$app" \
+                    -u normal \
+                    -h string:x-dunst-stack-tag:pavolume \
+                    -i "$icon_unmute" \
+                    "$app" "Audio ON:  $volume%"
             fi
         fi
         ;;
